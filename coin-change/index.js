2a45e868d9da171c1e7506692dbb8f096ab69048
Daniel Johnson <danielj41@gmail.com>
2018-03-10 21:42:17 -0800

    work in progress

--- coin-change/index.js
+++ coin-change/index.js
@@ -1,31 +1,90 @@
-((
-  yCombinator,
+console.log(((
+  yComb,
+  lift,
   withState,
   memoize,
   bind,
+  run,
 ) =>
+    ((
+      coinChange,
+    ) =>
+      
+      run({}, bind(
+        withState(() => ({ total: 100, coins: [22, 14, 7, 5, 1] })),
+        coinChange,
+      ))
 
-1
+    )(
+      yComb(coinChange =>
+        memoize(
+          ({state, value: { total, coins }}) => total === 0 ? lift([]) :
+
+            ((
+              findBest
+            ) =>
+
+
+                findBest({
+                  state,
+                  value: {
+                    loopCoins: coins.filter(coinAmount => coinAmount <= total),
+                    best: null
+                  }
+                })
+
+
+            )(
+              yComb(findBest => ({state, value: { loopCoins, best }}) => loopCoins.length === 0 ? lift(best) :
+                run(state,
+                  bind(
+                    withState(() => ({ total: total - loopCoins[0], coins })),
+                  bind(
+                    coinChange,
+                  bind(
+                    withState((cur) => ({
+                      loopCoins: loopCoins.slice(1),
+                      best: best === null || (cur !== null && cur.length + 1 < best.length) ?
+                              [loopCoins[0], ...cur]
+                            :
+                              best
+                    })),
+                    findBest
+                  )))
+                )
+              )
+            )
+
+
+
+        )
+      )
+    )
+  
 
 )(
   f => (x => f(y => x(x)(y)))(x => f(y => x(x)(y))),
 
-  f => ({ value, state }) => ({ value: f(value), {} }),
+  value => ({ value, state: {} }),
 
-  f => ({ value, state }) => 
-    ((key) =>
+  f => ({ value, state }) => ({ value: f(value), state: {} }),
+
+  f => ({ value, state }) =>
+    (key =>
       key in state ?
         { value: state[key], state: {} }
         :
-        (({ valF }) =>
-           ({ value: valF, {[key]: valF} })
-        )(f(value))
-    )(JSON.stringify(value))
+        (({ value: valF, state }) =>
+           ({ value: valF, state: {[key]: valF, ...state} })
+        )(f({ value, state} ))
+    )(JSON.stringify(value)),
 
-  (f, g) => (value, state) => 
-    (({ value: valF, state: stateF }) => 
-      (({ value: valG, state: stateG }) =>
-        ({ value: valG, state: {...stateF, ...stateG} })
-      )(g(valF, stateF)) 
-    )(f(value, state))
-)
+  (f, g) => ({ value, state }) => 
+    (({ value: valF, state: stateF }) =>
+      (({ value: valG, state: stateG }) => 
+        ({ value: valG, state: {...state, ...stateF, ...stateG} })
+      )(g({ value: valF, state: { ...state, ...stateF } })) 
+    )(f({ value, state })),
+
+  (initial, fn) => fn({ value: null, state: initial }),
+))
