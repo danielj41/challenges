180c0c12cfd069a0e9ab7838aae12725dbbf3481
Daniel Johnson <danielj41@gmail.com>
2018-03-11 14:20:00 -0700

    finish

--- coin-change/index.js
+++ coin-change/index.js
@@ -1,83 +1,82 @@
-console.log(((
-  yComb,
-  lift,
-  memoize,
-  bind,
-) =>
-    ((
-      coinChange,
-    ) =>
-      
-      
+console.log(
+  ((
+    Y,
+    lift,
+    bind,
+    memoize,
+  ) =>
+    (coinChange =>
+
+      // main
       bind(
         lift({ total: 100, coins: [22, 7, 5, 1] }),
-        coinChange,
+        coinChange
       )({})
 
     )(
-      yComb(coinChange =>
-        memoize(
-          ({ total, coins }) => total === 0 ? lift([]) :
 
-            ((
-              findBest
-            ) =>
+      // coinChange
+      Y(coinChange => memoize(({ total, coins }) =>
+        total === 0
+          ? lift([])
+          : (findBest =>
 
-                bind(
-                  lift({
-                    loopCoins: coins.filter(coinAmount => coinAmount <= total),
-                    best: null
-                  }),
-                  findBest
-                )
-
+              bind(
+                lift({
+                  loopCoins: coins.filter(coinAmount => coinAmount <= total),
+                  best: null,
+                }),
+                findBest,
+              )
 
             )(
-              yComb(findBest => ({ loopCoins, best }) => loopCoins.length === 0 ? lift(best) :
-                  bind(bind(bind(
-                    lift({ total: total - loopCoins[0], coins }),
-                    coinChange,
-                  ),
-                    cur => lift({
-                      loopCoins: loopCoins.slice(1),
-                      best: best === null || (cur !== null && cur.length + 1 < best.length) ?
-                              [loopCoins[0], ...cur]
-                            :
-                              best
-                    }),
-                  ),
-                    findBest
-                  )
-                
-              )
-            )
 
+              // findBest
+              Y(findBest => ({ loopCoins, best }) =>
+                loopCoins.length === 0
+                  ? lift(best)
+                  : bind(
+                    bind(
+                    bind(
+                      lift({ total: total - loopCoins[0], coins }),
+                      coinChange
+                    ),
+                      cur =>
+                        lift({
+                          loopCoins: loopCoins.slice(1),
+                          best:
+                            best === null ||
+                            (cur !== null && cur.length + 1 < best.length)
+                              ? [loopCoins[0], ...cur]
+                              : best
+                        })
+                    ),
+                      findBest
+                    ))))))
+  )(
 
+    // Y (combinator)
+    f => (x => f(y => x(x)(y)))(x => f(y => x(x)(y))),
 
-        )
-      )
-    )
-  
+    // lift
+    value => state => [value, {}],
 
-)(
-  f => (x => f(y => x(x)(y)))(x => f(y => x(x)(y))),
+    // bind
+    (a, b) => state =>
+      (([valueA, stateA]) =>
+        (([valueB, stateB]) =>
+          [valueB, { ...state, ...stateA, ...stateB }]
+        )(b(valueA)({ ...state, ...stateA })
+      ))(a(state)),
 
-  value => state => [value, {}],
-
-  f => value => state =>
-    (key =>
-      key in state ?
-          [state[key], {}]
-        : 
-        (([valueF, stateF]) =>
-           [valueF, {[key]: valueF, ...stateF}]
-        )(f(value)(state))
-    )(JSON.stringify(value)),
-
-  (f, g) => state => 
-    (([ valueF, stateF ]) =>
-      (([ valueG, stateG ]) =>
-        [valueG, {...state, ...stateF, ...stateG}]
-      )(g(valueF)({...state, ...stateF})) 
-    )(f(state))
-))
+    // memoize
+    f => value => state =>
+      (key =>
+        key in state
+          ? [state[key], {}]
+          : (([valueF, stateF]) =>
+              [valueF, { [key]: valueF, ...stateF }]
+            )(f(value)(state))
+      )(JSON.stringify(value))
+  )
+);
